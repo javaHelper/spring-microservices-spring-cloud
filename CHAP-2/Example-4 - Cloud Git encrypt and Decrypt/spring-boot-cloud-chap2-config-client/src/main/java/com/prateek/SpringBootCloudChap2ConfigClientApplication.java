e23a2901f719f9f289b3fe4b87a8055ee143968a
prateek <prateek.ashtikar@mastercard.com>
2020-01-04 23:07:17 +0530

    Spring Cloud Config Encrypt and Decrypt

---
+++ CHAP-2/Example-4 - Cloud Git encrypt and Decrypt/spring-boot-cloud-chap2-config-client/src/main/java/com/prateek/SpringBootCloudChap2ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.prateek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudChap2ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCloudChap2ConfigClientApplication.class, args);
+	}
+
+}
